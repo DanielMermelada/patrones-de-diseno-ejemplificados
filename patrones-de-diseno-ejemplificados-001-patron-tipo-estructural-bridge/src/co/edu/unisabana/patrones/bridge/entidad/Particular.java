f7ab7c872da8fb7d30f0fa87e516e7575caec105
GermanG55 <40509764+GermanG55@users.noreply.github.com>
2023-02-09 21:35:41 -0500

    Delete patrones-de-diseno-ejemplificados-001-patron-tipo-estructural-bridge directory

--- patrones-de-diseno-ejemplificados-001-patron-tipo-estructural-bridge/src/co/edu/unisabana/patrones/bridge/entidad/Particular.java
+++
@@ -1,8 +0,0 @@
-package co.edu.unisabana.patrones.bridge.entidad;
-
-import co.edu.unisabana.patrones.bridge.interfaz.Placa;
-
-public class Particular implements Placa {
-    @Override
-    public void anotar(){ System.out.println("Anotando una placa particular!"); }
-}
