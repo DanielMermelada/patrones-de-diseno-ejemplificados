85e7a65a7ae4fc8824a91e8c2f6ab82e198f4900
GermanG55 <40509764+GermanG55@users.noreply.github.com>
2023-02-09 21:34:37 -0500

    Delete patrones-de-diseno-ejemplificados-002-patron-tipo-creacional-builder/cereales-builder directory

--- patrones-de-diseno-ejemplificados-002-patron-tipo-creacional-builder/cereales-builder/src/co/edu/sabana/patron/builder/interfaz/CerealPlan.java
+++
@@ -1,8 +0,0 @@
-package co.edu.sabana.patron.builder.interfaz;
-
-public interface CerealPlan {
-    void setCerealOjuelas(String ojuelas);
-    void setCerealYogurt(String yogurt);
-    void setCerealQueso(String queso);
-    void setCerealFruta(String fruta);
-}
