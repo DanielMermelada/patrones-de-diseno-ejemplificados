3ba01e64a12f75ccd2f0d436b8520e5531635e2c
GermanG55 <40509764+GermanG55@users.noreply.github.com>
2023-02-09 21:32:56 -0500

    Volviendo a mandar archivos
    Soy guevon y borre los archivos intentando subir los otros

---
+++ patrones-de-diseno-ejemplificados-002-patron-tipo-creacional-builder/cereales-builder/src/co/edu/sabana/patron/builder/interfaz/CerealPlan.java
@@ -0,0 +1,8 @@
+package co.edu.sabana.patron.builder.interfaz;
+
+public interface CerealPlan {
+    void setCerealOjuelas(String ojuelas);
+    void setCerealYogurt(String yogurt);
+    void setCerealQueso(String queso);
+    void setCerealFruta(String fruta);
+}
